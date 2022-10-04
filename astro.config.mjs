155ad4424c145dd7edd34f13dddc1f35c4fa9167
ndeyros <ndeyros@lenovo.com>
2022-10-04 14:30:11 -0300

    initial commit & Astro setup

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import prefetch from "@astrojs/prefetch";
+import mdx from "@astrojs/mdx";
+import image from "@astrojs/image";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), prefetch(), mdx(), image(), sitemap()]
+});
