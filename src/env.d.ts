b2a469273a718673d8fd51634720fe73cf7716bb
Nicolas Deyros <ndeyros@Lenovo.com>
2023-02-28 17:35:43 -0300

    formik implementation

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,3 @@
 /// <reference path="../.astro/types.d.ts" />
-<<<<<<< HEAD
 /// <reference types="astro/client" />
-=======
 /// <reference types="@astrojs/image/client" />
->>>>>>> 5a4fe2bf71666e41e19c204f8eed00ca1713ed6d
