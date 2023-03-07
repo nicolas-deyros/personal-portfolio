1fcc53a1f9ddc0a19b7ca7742ab6854cc34cc162
Nicolas Deyros <ndeyros@Lenovo.com>
2023-03-07 17:24:41 -0300

    fix vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,23 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import image from '@astrojs/image'
+import prefetch from '@astrojs/prefetch'
+import mdx from '@astrojs/mdx'
+import vercel from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
-// https://astro.build/config
-import tailwind from '@astrojs/tailwind';
-
-// https://astro.build/config
-import image from '@astrojs/image';
-
-// https://astro.build/config
-import prefetch from '@astrojs/prefetch';
-
-// https://astro.build/config
-import mdx from '@astrojs/mdx';
-
-// https://astro.build/config
-import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
-import react from "@astrojs/react";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), prefetch(), mdx(), react()],
-  output: 'server',
-  adapter: vercel(analytics: true)
-});
+	integrations: [
+		tailwind(),
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp',
+		}),
+		prefetch(),
+		mdx(),
+		react(),
+	],
+	output: 'server',
+	adapter: vercel({
+		analytics: true,
+	}),
+})
