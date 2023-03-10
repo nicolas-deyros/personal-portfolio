8ea541ad9219711253dc88d47acb6b860e39d99a
Nicolas Deyros <ndeyros@Lenovo.com>
2023-03-10 13:44:42 -0300

    fix blog index page

--- src/schemas/index.jsx
+++ src/schemas/index.jsx
@@ -5,24 +5,24 @@ const emailRules =
 
 export const BasicSchema = Yup.object().shape({
   name: Yup.string()
-    .min(2, 'Name must be at least 2 characters long')
+    .min(4, 'Name must be at least 4 characters long')
     .max(50, 'Name must be no longer than 50 characters')
     .matches(/^[a-zA-Z ]+$/, 'Name must only contain letters and spaces')
-    .required('Name is a required field'),
+    .required('Name is a required field and cannot be empty'),
   email: Yup.string()
     .email('Please enter a valid email')
     .matches(emailRules, {
       message: 'You need to add a valid email',
       excludeEmptyString: true,
     })
-    .required('Email is a required field'),
+    .required('Email is a required field and cannot be empty'),
   msg: Yup.string()
     .min(10, 'Message must be at least 10 characters long')
     .max(500, 'Message must be no longer than 500 characters')
-    .matches(/^[\w\s\?\!\$\@\.\,]+$/, {
+    .matches(/^[áéíóúÁÉÍÓÚñÑ\w\s\?\!\$\@\.\,]+$/, {
       message:
         'The message can only contain letters, spaces and the following characters (?!,.$@)',
       excludeEmptyString: true,
     })
-    .required('Give a little context'),
+    .required('Message cannot be empty. Please give a little context'),
 })
